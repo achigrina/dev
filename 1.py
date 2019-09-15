12c47c49b1b94baa1754314f3a2084c9ab8146a7
stud543 <123@mail.ru>
2019-09-15 10:06:08 +0300

    С3

--- 1.py
+++ 1.py
@@ -4,4 +4,22 @@ a=[]
 for i in range(n):
     a.append(random.randint(-10,10))
 
-print(a)
+#int(a)
+neg=[]
+zero=[]
+pos=[]
+for el in a:
+    if el<0:
+        neg.append(el)
+    if el==0:
+        zero.append(el)
+    if el>0:
+        pos.append(el)
+
+
+print("Отрицательные числа")
+print(neg)
+print("Положительные числа")
+print(pos)
+print("Нули")
+print(zero)
